5fbfe1c095ea07c944daff137039d6d2da1793b0
F1rstQu33n <jeastman@pwscc.edu>
2014-12-07 14:07:00 -0900

    Adding Files to repo

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public bool boolean1 = false;
+	private bool boolean2 = true;
+
+	public int integer = 1;
+	public float floatingPoint = 1.1f;
+
+	// Use this for initialization
+	void Start () {
+	if (boolean1 == true) {
+						Debug.Log ("Awesome");
+				}
+		integer = 10;
+		floatingPoint = 5.4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		integer = integer += 1;
+		integer += 1;
+	}
+}
---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	public bool touchingGround;
+		public float sphereRadius = 0.5f;
+
+	public Vector3 bounceVelocity = new Vector3( 0, 5, 0);
+
+	// Use this for initialization
+	void Start () {
+		Function1();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		touchingGround = IsTouching ();
+			Debug.Log( IsTouching());
+
+		if (touchingGround == true) { 
+			Bounce( bounceVelocity ); 
+		}
+	}
+
+	void Function1 () {
+
+		float tempVar = 1.1f;
+		tempVar = 5.4f;
+	}
+
+	void Bounce (Vector3 bounceVelocity) {
+		rigidbody.velocity = bounceVelocity;
+	}
+
+	bool IsTouching (){
+				return Physics.Raycast (transform.position, Vector3.down, sphereRadius);
+	}
+}
